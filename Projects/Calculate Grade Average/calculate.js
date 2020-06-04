033d76384f7b38cd7434d85d7a59d884470f2b61
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-06-04 15:05:54 -0400

    Delete calculate.js

--- Projects/Calculate Grade Average/calculate.js
+++
@@ -1,9 +0,0 @@
-function calculate() {
-    var num1= parseInt(document.getElementById('numb1').value);
-    var num2= parseInt(document.getElementById('numb2').value);
-    var num3= parseInt(document.getElementById('numb3').value);
-    var num4= parseInt(document.getElementById('numb4').value);
-    var num5= parseInt(document.getElementById('numb5').value);
-    var num6= parseInt(document.getElementById('numb6').value);
-    document.getElementById('re2').value = (num1 + num2 + num3 + num4 + num5 + num6)/6;
-}
